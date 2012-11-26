1b75254ce5dccbe0686e4fa4bf32f8a14105b551
Jon Mead <jon@webmead.com>
2012-11-26 01:23:09 +0000

    added functioning blank application

--- lib/class/mivik_request.php
+++ lib/class/mivik_request.php
@@ -1,12 +1,28 @@
 <?php 
 class mivik_request extends mivik_magicMembers{
+	/**
+	 * 
+	 * @var mivik_url
+	 */
 	public $url;
 	public $activeController;
 	public $activeAction;
 	public $activeTemplate;
 	public $appBase;
+	/**
+	 * 
+	 * @var mivik_config
+	 */
 	public $config;
+	/**
+	 * 
+	 * @var mivik_session
+	 */
 	public $session;
 	public $actionOutput;
+	/**
+	 * 
+	 * @var mivik_args
+	 */
 	public $args;
 }
